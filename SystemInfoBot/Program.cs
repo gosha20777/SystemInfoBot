a819b3735f08aeaaa30bf431a73571c8d59dd726
Georgy Perevozchikov <gosha20777@live.ru>
2018-05-28 19:02:07 +0300

    0

--- SystemInfoBot/Program.cs
+++ SystemInfoBot/Program.cs
@@ -3,13 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace SystemInfoBot
 {
     class Program
     {
+        private static readonly TelegramBotClient Bot = new TelegramBotClient("Your API key");
         static void Main(string[] args)
         {
+            var me = Bot.GetMeAsync().Result;
+            Console.Title = me.Username;
+
+            Bot.OnMessage += BotOnMessageReceived;
+            Bot.OnMessageEdited += BotOnMessageReceived;
+            Bot.OnReceiveError += BotOnReceiveError;
+
+            Bot.StartReceiving(Array.Empty<UpdateType>());
+            Console.WriteLine($"Start listening for @{me.Username}");
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+
+            if (message == null || message.Type != MessageType.TextMessage) return;
+
+            switch (message.Text.Split(' ').First())
+            {
+                // send inline keyboard
+                case "/info":
+                    await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
+                    break;
+                default:
+                    const string usage = @"
+Usage:
+/info   - send system info
+/keyboard - send custom keyboard
+/photo    - send a photo
+/request  - request location or contact";
+
+                    await Bot.SendTextMessageAsync(
+                        message.Chat.Id,
+                        usage,
+                        replyMarkup: new ReplyKeyboardRemove());
+                    break;
+            }
+        }
+
+        private static void BotOnReceiveError(object sender, ReceiveErrorEventArgs receiveErrorEventArgs)
+        {
+            Console.WriteLine("Received error: {0} — {1}",
+                receiveErrorEventArgs.ApiRequestException.ErrorCode,
+                receiveErrorEventArgs.ApiRequestException.Message);
         }
     }
 }
